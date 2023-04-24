11c3703619d573a3af49509fd6df3b8b1e1513d6
Carlos-Herman <104588315+Carlos-Herman@users.noreply.github.com>
2023-04-24 14:20:15 +0200

    Eliminación de código repetido
    Debido a que las acciones de cifrar y descifrar son prácticamente iguales, se puede añadir un "modificador" como parámetro a la función, que hará avanzar o retroceder el desplazamiento de las letras, por lo que puede usarse una sola función para ambas.

--- vigenere.py
+++ vigenere.py
@@ -1,4 +1,6 @@
-def codificar_vigenere(descifrar , clave ,alfabeto):
+# cifrado vigenere
+# funciones
+def codificar_vigenere(descifrar, clave, alfabeto, direccion):
     clave_len = len(clave)
     descifrar_len = len(descifrar)
     alfabeto_len = len(alfabeto)
@@ -9,37 +11,23 @@ def codificar_vigenere(descifrar , clave ,alfabeto):
         if descifrar_char not in alfabeto:
             flag += descifrar_char
             continue
+
         clave_pos = alfabeto.index(clave_char)
         descifrar_pos = alfabeto.index(descifrar_char)
-        flag_pos = (descifrar_pos + clave_pos) % alfabeto_len
+        flag_pos = (descifrar_pos - clave_pos*direccion) % alfabeto_len
         flag_char = alfabeto[flag_pos]
         flag += flag_char
     return flag
+# la función de descodificar puede usar la anterior, cambiando el valor de dirección,
+# para que el desplazamiento de las letras sea en sentido opuesto
+def descodificarVigenere(descifrar, clave, alfabeto, direccion):
+    return codificar_vigenere(descifrar, clave, alfabeto, -direccion)
 
-def descodificar_vigenere(descifrar , clave ,alfabeto):
-    clave_len = len(clave)
-    descifrar_len = len(descifrar)
-    alfabeto_len = len(alfabeto)
-    flag = ""
-    for i in range(descifrar_len):
-        clave_char = clave[i % clave_len]
-        descifrar_char = descifrar[i % descifrar_len]
-        if descifrar_char not in alfabeto:
-            flag += descifrar_char
-            continue
-        clave_pos = alfabeto.index(clave_char)
-        descifrar_pos = alfabeto.index(descifrar_char)
-        flag_pos = (descifrar_pos - clave_pos) % alfabeto_len
-        flag_char = alfabeto[flag_pos]
-        flag += flag_char
-    return flag
-
-
-
+# programa principal
 alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-descodificar = "QqmiaiiiYmisqmwmxijs"
+textoCifrado = "QqmiaiiiYmisqmwmxijs"
 clave = "Vigenere"
-flag = descodificar_vigenere(descodificar, clave, alfabeto)
+flag = codificar_vigenere(textoCifrado, clave, alfabeto, 1)
 print(f"El texto oculto es --> {flag}")
-texto_codificado = codificar_vigenere(flag, clave, alfabeto)
+texto_codificado = descodificarVigenere(flag, clave, alfabeto, 1)
 print(f"volviendolo a codificar nos queda --> {texto_codificado}")
